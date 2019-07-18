b54b99fe56fbf0ea2a5a83e202893ae449da154f
Tammy <tammy@packet.com>
2019-07-18 16:10:09 -0400

    edited terraform file to match new syntax

---
+++ terraform/packet-gpu.tf
@@ -0,0 +1,28 @@
+variable "auth_token" {}
+
+provider "docker" {}
+
+provider "packet" {
+    auth_token = "${var.auth_token}"
+}
+
+locals {
+    project_id = "a8a360a5-f0a4-45cf-a61b-e90a682efe3e"
+    # UUID of your project
+}
+
+resource "packet_device" "tf-gpu" {
+    hostname            = "gpu-testing"
+    plan                = "g2.large.x86"
+    facilities            = ["dfw2"]
+    operating_system    = "ubuntu_16_04"
+    billing_cycle       = "hourly"
+    project_id          = "${local.project_id}"
+}
+
+resource "docker_container" "ubuntu" {
+    name = "gpu-container"
+    image = ""
+    start = true
+
+}
