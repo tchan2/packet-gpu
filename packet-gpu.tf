09d1797cae68d4ef9f5f517bd67ddda047909071
Tammy <tammy@packet.com>
2019-08-26 11:18:09 -0400

    updated facilities variable

--- packet-gpu.tf
+++ packet-gpu.tf
@@ -18,10 +18,11 @@ resource "packet_device" "tf-gpu" {
     project_id          = "${packet_project.tf_project.id}"
     hostname            = "${var.hostname}"
     plan                = "${var.plan}"
-    facilities          = ["${var.facility}"]
+    facilities          = "${var.facility}"
     operating_system    = "${var.os}"
     billing_cycle       = "${var.billing_cycle}"
     user_data           = "${data.template_file.tf_userdata.rendered}"
+    
 }
 
 # References a file with script of necessary installations after server is created.
