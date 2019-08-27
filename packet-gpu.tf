962f55f42d4a517634f834d0827312a8ce1abab6
Tammy <tammy@packet.com>
2019-08-27 17:56:34 -0400

    updated packet-gpu.tf to fit module

--- packet-gpu.tf
+++ packet-gpu.tf
@@ -12,20 +12,14 @@ resource "packet_project" "tf_project" {
   name = "New Project"
 }
 
-resource "packet_device" "tf-gpu" {
+module "packet-gpu" {
+  source = "./modules/packet-gpu"
   # If you already have a project_id, please change project_id to:
   # project_id          = "${var.project_id}"
   project_id       = "${packet_project.tf_project.id}"
   hostname         = "${var.hostname}"
   plan             = "${var.plan}"
-  facilities       = "${var.facility}"
-  operating_system = "${var.os}"
+  facility         = "${var.facility}"
+  os               = "${var.os}"
   billing_cycle    = "${var.billing_cycle}"
-  user_data        = "${data.template_file.tf_userdata.rendered}"
-
-}
-
-# References a file with script of necessary installations after server is created.
-data "template_file" "tf_userdata" {
-  template = "${file("scripts/tf_userdata.sh.tpl")}"
 }
